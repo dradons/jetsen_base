e5ad4c54cca0e3e93bccd48f9c63ddb9b0628758
yangyufa <yangyufa.@jetsen.cn>
2017-03-27 17:06:45 +0800

    test package init

---
+++ ppnssm/src/test/java/com/jetsen/HelloWorld.java
@@ -0,0 +1,19 @@
+package com.jetsen;
+
+import org.junit.Test;
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+	}
+	
+	@Test
+	public void testSystemOut(){
+		System.out.println("hello world!");
+	}
+
+}
